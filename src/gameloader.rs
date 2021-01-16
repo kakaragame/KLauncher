bca2f338c1b7e9460774dde906fb02a5f63b9b31
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-01-16 15:23:01 -0500

    Moved to YML

--- src/gameloader.rs
+++ src/gameloader.rs
@@ -1,18 +1,21 @@
+extern crate yaml_rust;
+
 use std::fs;
 use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
 use std::process::Command;
 
-use java_properties::read;
+use yaml_rust::{YamlEmitter, YamlLoader};
 
 pub fn load(game: &str, dir: &str, engine: &str, debug: bool) {
     let home = std::env::var("HOME").unwrap();
-    let mut f2 = File::open(Path::new(&home).join(".kakara").join("settings.properties"));
-    let dst_map1 = read(BufReader::new(f2.unwrap()));
-    let map = dst_map1.unwrap();
-    let option = map.get("java").unwrap();
-    let mut java_command = Command::new(option.as_str())
+    let path = Path::new(&home).join(".kakara").join("settings.yml");
+    let ymlString = fs::read_to_string(path);
+    let docs = YamlLoader::load_from_str(&*ymlString.unwrap()).unwrap();
+    let program = docs[0]["java"].as_str().unwrap();
+    println!("Using java {}", program);
+    let mut java_command = Command::new(program)
         .current_dir(dir)
         .arg("-cp").arg(engine).
         arg("-jar").arg(game).
