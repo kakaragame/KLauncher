5aa6734e29cec2881cffcf2ede5939ddb8423482
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2021-02-07 20:06:07 -0500

    It is working.

--- src/jenkins.rs
+++ src/jenkins.rs
@@ -2,19 +2,63 @@ use jenkins_api::JenkinsBuilder;
 use jenkins_api::job::{Job, ShortJob, CommonJob};
 use std::ptr::null;
 use std::error::Error;
+use reqwest::Url;
+use std::str::FromStr;
+use std::fs;
+use std::path::Path;
+use std::fs::{create_dir_all, File};
+use std::io::Write;
 
-pub fn getBranchURL(branch: &str) -> Result<ShortJob<CommonJob>, &str> {
+pub fn getBranchURL(branch: &str) -> Result<CommonJob, String> {
     let jenkins = JenkinsBuilder::new(env!("JENKINS_URL")).build().unwrap();
     let job = jenkins.get_job("Kakara Engine 2").unwrap().as_variant::<jenkins_api::job::WorkflowMultiBranchProject>().unwrap();
     let vec = job.jobs;
-    let mut value = Result::Err("Missing Data");
+    let mut value = Result::Err(format!("Unable to find branch: {}", branch));
     for x in vec {
         if x.name.eq(branch) {
-            value = Result::Ok(x);
+            value = Result::Ok(x.get_full_job(&jenkins).unwrap());
         }
     }
     value
 }
+
+pub fn get_build_url(job: CommonJob) -> String {
+    let jenkins = JenkinsBuilder::new(env!("JENKINS_URL")).build().unwrap();
+
+    let build = job.last_build.unwrap();
+    let build1 = build.get_full_build(&jenkins).unwrap();
+    build1.url
+}
+
+pub async fn download_engine_jar(url: &str) -> String {
+    let input = format!("{}artifact/{}", url, "files.txt");
+    let resp = reqwest::get(Url::from_str(input.as_str()).unwrap()).await.unwrap().text().await.unwrap();
+    let split = resp.lines();
+    let vec: Vec<&str> = split.collect();
+    let mut response: String = String::new();
+    for x in vec {
+        if x.contains(get_native_name()) {
+            let string = format!("{}artifact/archives/{}", url, x);
+            let result = reqwest::get(Url::from_str(string.as_str()).unwrap()).await.unwrap().bytes().await.unwrap();
+            fs::create_dir_all("engine").unwrap();
+            let buf1 = Path::new(std::env::current_exe().unwrap().parent().unwrap()).join("engine");
+            if !buf1.exists() {
+                create_dir_all(buf1);
+            }
+            let buf = Path::new(std::env::current_exe().unwrap().parent().unwrap()).join("engine").join(x);
+            println!("[DEBUG] Downloaded: {}", buf.as_path().as_os_str().to_str().unwrap());
+            if !buf.exists() {
+                let mut file = File::create(buf).unwrap();
+                file.write_all(result.as_ref()).unwrap();
+            }
+
+            response = String::from(x);
+            break;
+        }
+    }
+    response
+}
+
 
 fn get_native_name() -> &'static str {
     if cfg!(windows) {
--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,14 @@ use clap::{App, Arg};
 
 mod gameloader;
 mod osspec;
-mod engine;
 mod jenkins;
 
 fn main() {
-    let result = jenkins::getBranchURL("master");
-    println!("{}", result.unwrap().url);
     let matches = App::new("Kakara Game Launcher").
         version("1.0-SNAPSHOT").author("Wyatt Jacob Herkamp <wherkamp@kingtux.me>").about("Launches the Kakara game").
-        arg(Arg::with_name("game").short("g").long("game").value_name("JAR_FILE").help("Takes the Kakara client").takes_value(true).required(true)).
+        arg(Arg::with_name("game").short("g").long("game").value_name("JAR_FILE").help("Takes the Kakara client").takes_value(true).required(false)).
         arg(Arg::with_name("engine").short("e").long("engine").value_name("JAR_FILE").help("Takes the Kakara Engine").takes_value(true).required(false)).
-        arg(Arg::with_name("dir").short("w").long("working_dir").value_name("WORKING_DIRECTORY").help("What is the working directory for Kakara").takes_value(true).required(true)).
+        arg(Arg::with_name("dir").short("w").long("working_dir").value_name("WORKING_DIRECTORY").help("What is the working directory for Kakara").takes_value(true).required(false)).
         get_matches();
     let game_jar = matches.value_of("game").unwrap_or("client.jar");
     let engine_jar: String;
@@ -25,8 +22,11 @@ fn main() {
     if matches.is_present("engine") {
         engine_jar = matches.value_of("engine").unwrap_or("engine.jar").parse().unwrap();
     } else {
+        let result = jenkins::getBranchURL("master");
+        let job = result.unwrap();
+        let engine1 = jenkins::get_build_url(job);
         let runtime = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
-        let s = runtime.block_on(engine::download_latest_build(working_directory));
+        let s = runtime.block_on(jenkins::download_engine_jar(&*engine1));
         if s == "" {
             // If the engine version was not found.
             println!("[ERROR] Unable to download engine version. Please provide an engine build with --engine");
